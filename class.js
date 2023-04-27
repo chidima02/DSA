1e6448e6c46581daf8b9967c90f0c56d18d85fa0
chidima02 <chukwuyenumblessing02@gmail.com>
2023-04-27 18:52:48 +0100

    (class.js)

---
+++ class.js
@@ -0,0 +1,42 @@
+class Node{
+    constructor(val){
+        this.val=val;
+        this.next=null;
+    }
+}
+
+class Linklist01{
+    constructor(){
+        this.head=null;
+        this.tail=null;
+        this.length=0;
+    }
+
+    insert(val){
+        let node=new Node(val);
+        if(!this.head){
+            this.head=node;
+            this.tail=this.head;
+        }else{
+           this.tail.next=node;
+           this.tail=node; 
+        }
+        this.length++;
+        return this;
+    }
+    remove(){
+        let current= this.tail;
+        let newTail=current;
+        while(current.next){
+            this.tail=newTail;
+            this.tail=null;
+        }
+      this.length--;
+      return current;
+    }
+
+}
+
+let Linklist01=new Linklist01()
+console.log(Linklist01.insert(800));
+console.log(Linklist01.insert(4000));
