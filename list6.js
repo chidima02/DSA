fa9deb6496122aeb3f7346857c0d27f2f1238fff
chidima02 <chukwuyenumblessing02@gmail.com>
2023-04-27 21:13:25 +0100

    (list6.js)

---
+++ list6.js
@@ -0,0 +1,37 @@
+class Node{
+    constructor(val){
+        this.val=val;
+        this.next=null;
+    }
+}
+
+class Linklist12{
+    constructor(){
+        this.head=null;
+        this.tail=null;
+        this.length=0;
+    }
+insert (val){
+    let node=new Node(val);
+    if (!this.head){
+        this.head=node;
+        this.tail=this.head;
+
+    }else{
+        this.tail.next=node;
+        this.tail=node;
+    }
+    this.length++
+    return this;
+ 
+}
+remove(){
+    let current= this.tail;
+    let newTail=current;
+    while(current.next){
+        this.tail=newTail;
+    }
+    this.length--;
+    return current;
+}
+}
